2645b26a28e33623326b305c53aae47492f46259
Seb Roffey <sebastian.roffrey@raytheon.co.uk>
2020-10-19 16:15:28 +0100

    Fully functioning program which can register, log in and edit user details on a database. Does not check for duplicate email and username when editing yet. Will program a function to check for duplicate entries for this reason, as well as to simplify registering.

--- app.py
+++ app.py
@@ -4,19 +4,17 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 import _sqlite3
 from DB import manipulateDB
 
+app = Flask(__name__)
+manipulateDB.innitializeUser_DB()
 
-# session stores values in a dict
-def loggedIn():
-    if session.get("user_id"):
-        return True
-    else:
-        return False
+DB_Location = "DB/User_Data.db"
+app.config["SECRET_KEY"] = "secretkey"
+app.config["SESSION_PERMANENT"] = False
 
 
 # using code to hash passwords with salt https://www.pythoncentral.io/hashing-strings-with-python/
 import uuid
 import hashlib
-
 
 def hash_password(password):
     # uuid is used to generate a random number
@@ -33,11 +31,41 @@ def check_password(hashed_password, entered_password):
         return False
 
 
-app = Flask(__name__)
-manipulateDB.innitializeUser_DB()
 
-DB_Location = "DB/User_Data.db"
-app.config["SECRET_KEY"] = "secretkey"
+# session stores values in a dict
+def loggedIn():
+
+    if session.get("user_id"):
+        return True
+    else:
+        return False
+
+
+def refreshSession(userID):
+    data = (userID,)
+
+    with manipulateDB.openDB(DB_Location) as db:
+        db.execute("SELECT username, forename, surname, email FROM Users WHERE userID = ?", data)
+        user = db.fetchone()
+
+    session["username"] = user[0]
+    session["forename"] = user[1]
+    session["surname"] = user[2]
+    session["email"] = user[3]
+    session["user_id"] = userID
+
+
+def validLogin(userID):
+
+    refreshSession(userID)
+    flash("Correct Password, Welcome " + session.get("forename"), "success")
+    return render_template('userPage.html')
+
+def vertifyUniqueEntry(entry, entryType):
+
+
+
+
 
 
 @app.route("/")
@@ -132,8 +160,8 @@ def loginUser():
                     storedHash = db.fetchone()[0]
                     db.execute("SELECT userID FROM Users WHERE email = ?", usernameOrEmail)
                     userID = db.fetchone()[0]
-                    db.execute("SELECT username FROM Users WHERE email = ?", usernameOrEmail)
-                    username = db.fetchone()[0]
+
+
 
 
 
@@ -156,15 +184,12 @@ def loginUser():
                     storedHash = db.fetchone()[0]
                     db.execute("SELECT userID FROM Users WHERE username = ?", usernameOrEmail)
                     userID = db.fetchone()[0]
-                username = usernameOrEmail[0]
+
 
         if check_password(storedHash, request.form.get("password")) == True:
 
             # Passwords match
-            flash("Correct Password", "success")
-            session["user_id"] = userID
-            session["username"] = username
-            return render_template('userPage.html')
+            return validLogin(userID)
 
         else:
 
@@ -175,10 +200,86 @@ def loginUser():
     return render_template('login.html')
 
 
+@app.route("/editUserDetails", methods=["GET", "POST"])
+def editUserDetails():
+    if loggedIn() == True:
+
+        if request.method == "POST":
+
+            username = request.form.get("username")
+            email = request.form.get("email")
+            forename = request.form.get("forename")
+            surname = request.form.get("surname")
+
+
+            # Iterate across array to save retyping code
+            infoArray = [["username", username], ["email", email], ["forename", forename], ["surname", surname]]
+            print(infoArray)
+
+            for index in range(0, len(infoArray)):
+
+                if infoArray[index][1] != '':
+
+                    data = (infoArray[index][1], session.get("user_id"),)
+
+                    with manipulateDB.openDB(DB_Location) as db:
+                        db.execute("UPDATE Users SET {} = ? WHERE userID = ?".format(infoArray[index][0]), data)
+
+                    session[infoArray[index][0]] = infoArray[index][1]
+                    flash(infoArray[index][0] + " changed successfully to " + infoArray[index][1], "success")
+
+
+            return render_template("editUserDetails.html")
+
+        else:
+            return render_template("editUserDetails.html")
+    else:
+        # not logged in
+        return render_template("login.html")
+
+
+@app.route("/changePassword")
+def changePassword():
+    if loggedIn() == True:
+
+        if request.method == "POST":
+
+            print()
+
+
+        else:
+            return render_template("editUserDetails.html")
+    else:
+        # not logged in
+        return render_template("login.html")
+
+
+@app.route("/deleteUser")
+def deleteUser():
+    if loggedIn() == True:
+
+        if request.method == "POST":
+
+            print()
+
+
+        else:
+            return render_template("editUserDetails.html")
+    else:
+        # not logged in
+        return render_template("login.html")
+
+
 @app.route("/logout")
 def logout():
-    session.pop("user_id")
+    if session.get("user_id") is not None:
 
-    flash("Logged out " + session.get("username"))
-    session.pop("username")
-    return render_template("home.html")
+        session.pop("user_id")
+        flash("Logged out " + session.get("username"))
+        session.pop("username")
+        return render_template("home.html")
+
+    else:
+
+        flash("Please log in.", "danger")
+        return render_template("login.html")
